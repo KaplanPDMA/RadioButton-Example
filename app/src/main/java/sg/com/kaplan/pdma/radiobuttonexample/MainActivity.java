c743a72961c5c4281a99eb9425fc29712d4646b1
William Ku <wku@kaplan.com>
2016-03-07 21:53:41 +0800

    Initial commit

---
+++ app/src/main/java/sg/com/kaplan/pdma/radiobuttonexample/MainActivity.java
@@ -0,0 +1,51 @@
+package sg.com.kaplan.pdma.radiobuttonexample;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    RadioGroup radioGroup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+        radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+    }
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch (view.getId()) {
+            case R.id.radioButtonBoy:
+                if (checked) {
+                    imageView.setImageResource(R.drawable.boy);
+                }
+                break;
+            case R.id.radioButtonGirl:
+                if (checked) {
+                    imageView.setImageResource(R.drawable.girl);
+                }
+                break;
+        }
+
+        radioGroup.clearCheck();        //clear all selections
+        radioGroup.check(view.getId()); //target
+    }
+
+}
